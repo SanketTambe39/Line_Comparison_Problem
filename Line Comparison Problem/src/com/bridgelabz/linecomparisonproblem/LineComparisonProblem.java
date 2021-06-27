53ec3cf6e04ffc1e47996c7a4386a1b55b1f75b7
SanketTambe39 <sankettambe39@gmail.com>
2021-06-27 23:02:24 +0530

    ADDED: Line Comparison added

--- Line Comparison Problem/src/com/bridgelabz/linecomparisonproblem/LineComparisonProblem.java
+++ Line Comparison Problem/src/com/bridgelabz/linecomparisonproblem/LineComparisonProblem.java
@@ -4,7 +4,8 @@ public class LineComparisonProblem {
 
 	public static void main(String[] args) {
 		
-		int line1x1=0;
+		System.out.println( "Welcome to Line Comparison Computation" );
+        int line1x1=0;
         int line1y1=0;
         
         int line1x2=0;
@@ -17,7 +18,7 @@ public class LineComparisonProblem {
         int line2y1=0;
         
         int line2x2=0;
-        int line2y2=8;
+        int line2y2=10;
         
         Double LengthOfLine2  = Math.sqrt(Math.pow((line2x2-line2x1),2) + Math.pow((line2y2-line2y1),2));
         
@@ -31,6 +32,17 @@ public class LineComparisonProblem {
         	System.out.println("The Two Lines Are Not Equal");
         }
 
+        int compare = LengthOfLine1.compareTo(LengthOfLine2);
+        if ( compare == 0) {
+        	System.out.println("The Two Line are equal");
+        }
+        else if(compare < 0){
+        	System.out.println("Line 2 is greater than Line 1 ");
+        }
+        else
+        {
+        	System.out.println("Line 1 is greater than Line 2");
+        }
 	}
 
 }
